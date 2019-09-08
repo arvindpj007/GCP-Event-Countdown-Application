8b57aceec30f47afd2a5954fe3ea56bf6df33f06
arvindpj007 <arvindpjayan@google.com>
2019-09-08 14:40:04 -0400

    Initial Commit

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1',port=8080,debug=True)
