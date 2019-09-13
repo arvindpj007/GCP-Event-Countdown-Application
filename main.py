3473401941ef249badf6a9bf0f0049a2cc7d85d8
arvindpj007 <arvindpjayan@google.com>
2019-09-12 20:08:02 -0400

    tested git

--- main.py
+++ main.py
@@ -1,12 +1,134 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, jsonify, request
+from google.cloud import datastore
 
-app = Flask(__name__,static_url_path='')
+app = Flask(__name__, static_url_path='')
+
+DS = datastore.Client(project='events-252318')
+EVENT = 'Event'  # Name of the event table, can be anything you like.
+ROOT = DS.key('Entities', 'root')  # Name of root key, can be anything.
 
 
 @app.route('/')
 def hello_world():
-    # return render_template('index.html')
-    return send_from_directory('','index.html')
+    """ This is the default page.
+        get:
+            summary: / endpoint.
+            description: renders static file index.html.
+            responses:
+                200:
+                    description: The events list and the option to add a new event.
+        """
+    return app.send_static_file('index.html')
+
+
+@app.route('/events')
+def events():
+    """ Returns all the events from cloudstore.
+        get:
+            summary: events endpoint.
+            description: Gets all the event entities and returns them as json .
+            responses:
+                200:
+                    description: json object to be returned.
+                    schema: [{name:"",date:""}]
+                404:
+                    description: Error : null.
+        """
+    query = DS.query(kind=EVENT, ancestor=ROOT).fetch()
+    event_list = []
+    for val in query:
+        event_list.append(dict(name=val.get('name'), date=val.get('date')))
+    # print(len(event_list))
+    return jsonify(events=event_list)
+
+
+@app.route('/event', methods=['POST'])
+def event():
+    """ To push events to cloud store
+        get:
+            summary: event endpoint.
+            description: Get name and date through ajax and stores it as an entity in cloud store and returns ID.
+            parameters:
+                - name: name
+                  in: request
+                  description: Event Name
+                  type: string
+                  required: true
+                  - name: date
+                    in: request
+                    description: Event ETA
+                    type: string
+                    required: true
+            responses:
+                200:
+                    description: Event ID to be returned.
+                    schema: string
+        """
+    para = request.get_data().decode('UTF-8').split()
+    name = para[0]
+    r_date = para[1]
+    [y, m, d] = r_date.split('-')
+    date = d + '-' + m + '-' + y
+    x = put_event(name, date)
+    return str(x)
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    """ To implicitly delete events from cloud store
+            get:
+                summary: delete endpoint.
+                description: Get name and date through ajax and use it to delete the event from cloud store.
+                parameters:
+                    - name: name
+                      in: request
+                      description: Event Name
+                      type: string
+                      required: true
+                      - name: date
+                        in: request
+                        description: Event ETA
+                        type: string
+                        required: true
+                responses:
+                    200:
+                        description: Event will bbe deleted from cloud store.
+            """
+    para2 = request.get_data().decode('UTF-8').split()
+    name = para2[1]
+    r_date = para2[0]
+    delete_event(name, r_date)
+    return "1"
+
+
+def put_event(name, date_str):
+    """Put the event into the cloud store using the given parameters name and date"""
+    # print('putting')
+    key = DS.key(EVENT, parent=ROOT)
+    entity = datastore.Entity(key=key)
+    entity.update({'name': name, 'date': date_str})
+    DS.put(entity)
+    return id(DS.key)
+
+
+def delete_event(name, date):
+    """Delete the event into the cloud store using the given parameters name and date"""
+    query = DS.query(kind=EVENT, ancestor=ROOT)
+    query.add_filter('name', '=', name)
+    query.add_filter('date', '=', date)
+    next_entity = query.fetch()
+    for val in next_entity:
+        DS.delete(val.key)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    """Run the app"""
+    app.run()
+
+# def fetch_events():
+#     print('fetching')
+#     query = DS.query(kind=EVENT, ancestor=ROOT).fetch()
+#     event_list = []
+#     for val in query:
+#         event_list.append(dict(name=val.get('name'), date=val.get('date').date()))
+#     print(event_list)
---
+++ static/script.js
@@ -0,0 +1,129 @@
+
+function reqJSON(method, url, data) {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve({status: xhr.status, data: xhr.response});
+      } else {
+        reject({status: xhr.status, data: xhr.response});
+      }
+    };
+    xhr.onerror = () => {
+      reject({status: xhr.status, data: xhr.response});
+    };
+    xhr.send(data);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  reqJSON('GET', '/events')
+  .then(({status, data}) => {
+    // Use the *data* argument to change what we see on the page.
+    // It will look something like this:
+    // {
+    //   "events": [
+    //     {"name": "Grandma's Birthday", "date": "08-05"},
+    //     {"name": "Independence Day", "date": "07-04"}
+    //   ]
+    // }
+
+    // There are better ways, but this is illustrative of the concept:
+    setInterval(function () {
+        let html = '<table>' +
+            '<tr>\n' +
+            '    <th>Event Name</th>\n' +
+            '    <th>Event Date (dd-mm-yyyy)</th> \n' +
+            '    <th>Time Left (d:h:m:s)</th>\n' +
+            '  </tr>';
+    for (let event of data.events) {
+
+        var datest = event.date.split('-');
+        var target = new Date(+datest[2], datest[1]-1, +datest[0]);
+        var time = Math.floor((+target - new Date()) / 1000);
+
+        if(time<=0)
+        {
+           //console.log('negative countdown')
+            $.ajax({
+            type: "POST",
+            url: "/delete",
+            dataType: "json",
+            data: event.date.toString()+' '+event.name.toString(),
+            success: function (data) {
+                console.log('Success');
+                alert('The date you have entered is passed, hence it was automatically deleted')
+                document.location.reload();
+            },
+            error: function () {
+                console.log('Error');
+            }
+        });
+        }
+        else
+        {
+            var day = Math.floor(time / (24 * 3600));
+            time = time % (24 * 3600);
+            var hour = Math.floor(time / 3600);
+            time %= 3600;
+            var minute = Math.floor(time / 60);
+            time %= 60;
+            var second = Math.floor(time);
+
+            html += `<tr><td>${event.name} </td> <td> ${event.date} </td><td> ${day}:${hour}:${minute}:${second}</td> </tr>`;
+        }
+    }
+    document.getElementById('events').innerHTML = html;
+
+    }, 1000);
+
+
+  })
+  .catch(({status, data}) => {
+    // Display an error.
+    document.getElementById('events').innerHTML = 'ERROR: ' +
+      JSON.stringify(data);
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded',() =>{
+
+  document.getElementById('create').addEventListener("click",()=> {
+    if( !document.getElementById('name').value || !document.getElementById('date').value)
+        alert("either name or date is not entered ");
+    else {
+        $.ajax({
+            type: "POST",
+            url: "/event",
+            dataType: "json",
+            data: document.getElementById('name').value + ' ' + document.getElementById('date').value,
+            success: function (data) {
+                console.log('Success');
+                alert("Event created with id: "+data+" name: " + document.getElementById('name').value + " and date: " + document.getElementById('date').value);
+                document.location.reload();
+            },
+            error: function () {
+                console.log('Error');
+            }
+        });
+    }
+  });
+});
+
+// function listner()
+// {
+//   const create_button = document.getElementById('create');
+//   if(create_button)
+//   {
+//     var name= document.getElementById('name');
+//     var date= document.getElementById('date');
+//     create_button.addEventListener("click",function(){alert(name.value+date.value);});
+//   }
+//   else
+//   {
+//     alert('error');
+//   }
+// }
